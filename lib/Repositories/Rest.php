52536d5f16506926d165e29fa68ffcc7fa82d6f8
Alex Standiford <alex@standiford.us>
2023-10-10 22:43:57 -0400

    REST Router Support

--- lib/Repositories/Rest.php
+++ lib/Repositories/Rest.php
@@ -4,6 +4,7 @@ namespace Phoenix\Core\Repositories;
 
 use Phoenix\Core\Container;
 use Phoenix\Core\Exceptions\DiException;
+use Phoenix\Rest\Interfaces\Validation;
 
 class Rest
 {
@@ -11,13 +12,14 @@ class Rest
      * Register a GET route with the router.
      *
      * @param string $endpoint The URL pattern of the route.
+     * @param Validation[] $validations List of validations to confirm this is valid.
      * @param callable $callback The callback to invoke when this route is matched.
      * @return void
      */
-    public static function get(string $endpoint, callable $callback): void
+    public static function get(string $endpoint, array $validations, callable $callback): void
     {
         try {
-            Container::rest()->get($endpoint, $callback);
+            Container::rest()->get($endpoint, $validations, $callback);
         } catch (DiException $e) {
         }
     }
@@ -26,13 +28,14 @@ class Rest
      * Register a POST route with the router.
      *
      * @param string $endpoint The URL pattern of the route.
+     * @param Validation[] $validations List of validations to confirm this is valid.
      * @param callable $callback The callback to invoke when this route is matched.
      * @return void
      */
-    public static function post(string $endpoint, callable $callback): void
+    public static function post(string $endpoint, array $validations, callable $callback): void
     {
         try {
-            Container::rest()->post($endpoint, $callback);
+            Container::rest()->post($endpoint, $validations, $callback);
         } catch (DiException $e) {
         }
     }
@@ -41,13 +44,14 @@ class Rest
      * Register a PUT route with the router.
      *
      * @param string $endpoint The URL pattern of the route.
+     * @param Validation[] $validations List of validations to confirm this is valid.
      * @param callable $callback The callback to invoke when this route is matched.
      * @return void
      */
-    public static function put(string $endpoint, callable $callback): void
+    public static function put(string $endpoint, array $validations, callable $callback): void
     {
         try {
-            Container::rest()->put($endpoint, $callback);
+            Container::rest()->put($endpoint, $validations, $callback);
         } catch (DiException $e) {
         }
     }
@@ -56,13 +60,14 @@ class Rest
      * Register a DELETE route with the router.
      *
      * @param string $endpoint The URL pattern of the route.
+     * @param Validation[] $validations List of validations to confirm this is valid.
      * @param callable $callback The callback to invoke when this route is matched.
      * @return void
      */
-    public static function delete(string $endpoint, callable $callback): void
+    public static function delete(string $endpoint, array $validations, callable $callback): void
     {
         try {
-            Container::rest()->delete($endpoint, $callback);
+            Container::rest()->delete($endpoint, $validations, $callback);
         } catch (DiException $e) {
         }
     }
@@ -71,13 +76,14 @@ class Rest
      * Register a PATCH route with the router.
      *
      * @param string $endpoint The URL pattern of the route.
+     * @param Validation[] $validations List of validations to confirm this is valid.
      * @param callable $callback The callback to invoke when this route is matched.
      * @return void
      */
-    public static function patch(string $endpoint, callable $callback): void
+    public static function patch(string $endpoint, array $validations, callable $callback): void
     {
         try {
-            Container::rest()->patch($endpoint, $callback);
+            Container::rest()->patch($endpoint, $validations, $callback);
         } catch (DiException $e) {
         }
     }
@@ -86,13 +92,14 @@ class Rest
      * Register an OPTIONS route with the router.
      *
      * @param string $endpoint The URL pattern of the route.
+     * @param Validation[] $validations List of validations to confirm this is valid.
      * @param callable $callback The callback to invoke when this route is matched.
      * @return void
      */
-    public static function options(string $endpoint, callable $callback): void
+    public static function options(string $endpoint, array $validations, callable $callback): void
     {
         try {
-            Container::rest()->options($endpoint, $callback);
+            Container::rest()->options($endpoint, $validations, $callback);
         } catch (DiException $e) {
         }
     }
