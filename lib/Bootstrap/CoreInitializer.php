dddbafa8db57877246a2613da11d9587b2c660c4
Alex Standiford <alex@standiford.us>
2024-03-18 08:13:39 -0400

    Progress

--- lib/Bootstrap/CoreInitializer.php
+++ lib/Bootstrap/CoreInitializer.php
@@ -5,14 +5,21 @@ namespace PHPNomad\Core\Bootstrap;
 use PHPNomad\Core\Facades\Cache;
 use PHPNomad\Core\Facades\Event;
 use PHPNomad\Core\Facades\Logger;
+use PHPNomad\Core\Strategies\InstanceProviderStrategy;
 use PHPNomad\Core\Strategies\Logger as LoggerStrategy;
+use PHPNomad\Di\Interfaces\CanSetContainer;
+use PHPNomad\Di\Interfaces\InstanceProvider;
+use PHPNomad\Di\Traits\HasSettableContainer;
 use PHPNomad\Facade\Interfaces\HasFacades;
 use PHPNomad\Loader\Interfaces\HasClassDefinitions;
 use PHPNomad\Loader\Interfaces\HasLoadCondition;
+use PHPNomad\Loader\Interfaces\Loadable;
 use PHPNomad\Logger\Interfaces\LoggerStrategy as CoreLoggerStrategy;
 
-final class CoreInitializer implements HasLoadCondition, HasFacades, HasClassDefinitions
+final class CoreInitializer implements HasLoadCondition, HasFacades, HasClassDefinitions, CanSetContainer, Loadable
 {
+    use HasSettableContainer;
+
     public const REQUIRED_PHP_VERSION = '7.4';
     /**
      * @var string
@@ -48,5 +55,10 @@ final class CoreInitializer implements HasLoadCondition, HasFacades, HasClassDef
     public function getClassDefinitions(): array
     {
         return [LoggerStrategy::class => CoreLoggerStrategy::class];
+    }
+
+    public function load(): void
+    {
+        $this->container->bindSingletonFromFactory(InstanceProvider::class, fn() => new InstanceProviderStrategy($this->container));
     }
 }
